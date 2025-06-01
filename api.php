7214a21cae640cb079ad4c95859a784d74c3d8ac
polina <pbarmakova@gmail.com>
2025-06-01 11:37:26 +0300

    front 1 station

---
+++ api.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require_once 'db.php';
+$pdo = getPDO();
+
+try {
+    // Получаем станции
+    $stations = $pdo->query("SELECT id, name, x, y FROM stations")->fetchAll();
+
+    // Получаем соединения
+    $connections = $pdo->query("SELECT station_id1, station_id2 FROM connections")->fetchAll();
+
+    echo json_encode([
+        "stations" => $stations,
+        "connections" => $connections
+    ], JSON_UNESCAPED_UNICODE);
+} catch (PDOException $e) {
+    echo json_encode(["error" => $e->getMessage()]);
+}
